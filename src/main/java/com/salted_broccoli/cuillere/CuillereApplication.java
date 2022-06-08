0788c58675abd031bcb6e048f4124cf425d58b59
salted-brocco-lee <benoit.hert@hotmail.fr>
2022-06-08 09:17:24 +0200

    Initial commit

---
+++ src/main/java/com/salted_broccoli/cuillere/CuillereApplication.java
@@ -0,0 +1,13 @@
+package com.salted_broccoli.cuillere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuillereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuillereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salted_broccoli/cuillere/CuillereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salted_broccoli.cuillere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuillereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
